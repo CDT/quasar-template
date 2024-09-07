51842f23d965aba2bb4b739e3a25eb87935fc2ee
CDT <779888925@qq.com>
2024-09-07 15:57:24 +0800

    .

--- front/src/pages/QueryDepts.vue
+++ front/src/pages/QueryDepts.vue
@@ -10,7 +10,7 @@
         <q-select v-model="selectedType" :options="departmentTypes" label="科室类型" outlined emit-value map-options />
       </div>
       <div class="col-12 col-md-2 self-center">
-        <q-btn color="primary" icon="search" @click="searchDepartments(pagination.page)" />
+        <q-btn color="primary" icon="search" @click="searchDepartments" />
       </div>
     </div>
 
@@ -19,9 +19,27 @@
         :columns="columns" row-key="id"
         :loading="loading" v-model:pagination="pagination"
         @request="onRequest">
+        <!-- Loading -->
         <template v-slot:loading>
           <q-inner-loading showing color="primary" />
         </template>
+
+        <!-- Expand button -->
+        <template v-slot:body-cell-expansion="props">
+          <q-td auto-width>
+            <q-btn size="sm" color="accent" round dense @click="console.log('expand')" icon="add" />
+          </q-td>
+        </template>
+
+        <template v-slot:body-cell-actions="props">
+          <q-td :props="props">
+            <div class="q-pa-md q-gutter-sm">
+              <q-btn color="primary" outline icon="edit" size="sm" @click="console.log(props.row)" />
+              <q-btn color="negative" outline icon="delete" size="sm" @click="console.log(props.row)" />
+            </div>
+          </q-td>
+        </template>
+
       </q-table>
     </div>
   </q-page>
@@ -46,12 +64,14 @@ const pagination = ref<any>({
 const departmentTypes = ref<SelectOption[]>([])
 
 const columns: QTableColumn[] = [
+  { name: 'expansion', label: '+', field: 'expand', align: 'center' },
   { name: 'area_name', label: '院区', field: 'area_name', sortable: true, align: 'center' },
   { name: 'name', label: '名称', field: 'name', sortable: true, align: 'center' },
   { name: 'addr', label: '地址', field: 'addr', sortable: true, align: 'center' },
   { name: 'type_name', label: '类型', field: 'type_name', align: 'center' },
   { name: 'phone', label: '电话', field: 'phone', align: 'center' },
-  { name: 'sup_dept_name', label: '上级科室', field: 'sup_dept_name', align: 'center' }
+  { name: 'sup_dept_name', label: '上级科室', field: 'sup_dept_name', align: 'center' },
+  { name: 'actions', label: '操作', field: 'actions', align: 'center' }
 ]
 
 const filter = computed(() => {
@@ -61,19 +81,19 @@ const filter = computed(() => {
   }
 })
 
-const searchDepartments = async (page = 1) => {
+const searchDepartments = async () => {
   loading.value = true
   try {
     const resp = await api.get('/depts',
       { params: {
           keyword: keyword.value,
           type: selectedType.value,
-          page: page,
+          page: pagination.value.page,
           per_page: pagination.value.rowsPerPage
         }
       })
     departments.value = resp.data?.data
-    pagination.value.page = page
+    // pagination.value.page = page
     pagination.value.rowsNumber = resp.data?.total || 0
   } catch (error: any) {
     showNotification(error.message, 'negative')
@@ -84,7 +104,7 @@ const searchDepartments = async (page = 1) => {
 
 const onRequest = (props: any) => {
   const { page } = props.pagination
-  searchDepartments(page)
+  searchDepartments()
 }
 
 // 初始化数据
@@ -93,5 +113,7 @@ api.get('/options?dict=ORG_TYPE').then( (resp: any) => {
 }).catch( (error: any) => {
   showErrorRespNotification(error)
 })
+
+searchDepartments()
 
 </script>
